5a80a93162eb6b4bec444394a9b6a767ed08e781
AndBarata <146003937+AndBarata@users.noreply.github.com>
2023-10-10 19:29:01 +0100

    FIm da aula 10/10

--- tp2/work2_Real.m
+++ tp2/work2_Real.m
@@ -70,40 +70,39 @@ end
 figure(2); plot(10*log(energy),'color', dark_green); xlabel("Frame"); ylabel('Energia (dB)'); grid on; title("Energia por Frame"); drawnow;
 figure(3); plot(zeroCrossingRate, 'color', dark_green); xlabel("Frame"); ylabel('Crossing Rate'); grid on; title("Crossing Rate por Frame"); drawnow;
 figure(4); plot(f0, 'color', dark_green); xlabel("Frame"); ylabel('Frequency'); grid on; title("Fundamental frequency (f0)"); drawnow;
-figure(5); plot(t_y, f0, 'color', dark_green); xlabel("Frame"); ylabel('Frequency'); grid on; title("Fundamental frequency in y(f0)"); drawnow;
 
 
 fprintf("Total de frames em silencio %f \n", totalframes_em_silencio);
 fprintf("End of the program.\n")
 
 % Define a function to calculate F0 using autocorrelation
-function [f0, totalframes_em_silencio] = calculateF0Autocorrelation(frame, fs, totalframes_em_silencio)
-% We need to search the second peak, because the first peak is always 0 
-% The voice of man is between 70 and 250 Hz, so we need to find the peak in this interval
-
-   fo_min = 70; %Hz
-   fo_max = 250; %Hz
-
-%   fo_test = floor((1/fo_min)*fs);
-%   fo_test2 = floor((1/fo_max)*fs);
-%   length_frame = length(frame);
-%   fprintf("Tamanho %f Minimo %f Máximo %f \n", length_frame, fo_test, fo_test2);
-
-    % Calculate autocorrelation
+function [f0, number_frame] = calculateF0Autocorrelation(frame, fs, number_frame)
     autocorr = xcorr(frame);
-
-    autocorr1 = autocorr((length(frame) + floor((1/fo_max)*fs)):(length(frame) + floor((1/fo_min)*fs)));
+    fprintf("\nLength frame: %d\nLength corr: %d", length(frame), length(autocorr));
     
+    dark_blue = 1/255 * [3,37,126];
+
+
+    % We need to search the second peak, because the first peak is always 0 
+    % The voice of man is between 70 and 250 Hz, so we need to find the peak in this interval
+    fo_min = 70; %Hz
+    fo_max = 250; %Hz
+
+    
+    % Calculate autocorrelation
+    autocorr2 = autocorr(length(frame) + floor(fs/fo_max) :length(frame) + floor(fs/fo_min));
+
+
     % Find the index of the maximum peak in the autocorrelation function
-    [~, maxIdx] = max(autocorr1);
-    
-    fprintf("Indice %f \n", maxIdx);
-
+    [~, maxIdx] = max(autocorr2);
     % Calculate F0 using the index
-    f0 = fs / maxIdx;
-    
-    if(f0>250)
-        totalframes_em_silencio = totalframes_em_silencio + 1;
+    f0 = fs / (maxIdx + floor(fs/fo_max));
+    fprintf("Frequencia fundamental [%d]: %f\n", maxIdx, f0);
+    number_frame = number_frame + 1;
+    if(number_frame==1281)
+        figure(10); 
+        subplot(1,2,1); plot(autocorr, 'color', dark_blue); xlabel("Indice"); ylabel("Magnitude"); title("Corr. frame 1281");
+        subplot(1,2,2); plot((length(frame) + floor(fs/fo_max) : length(frame) + floor(fs/fo_min)) ,autocorr(length(frame) + floor(fs/fo_max) : length(frame) + floor(fs/fo_min)), 'color', dark_blue); xlabel("Indice"); ylabel("Magnitude"); title("Corr. frame 1281 cortada");
     end
 end
 
