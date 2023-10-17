3e556c97594e378b9d87a4420df83a7e104ac04d
Domandrenog <andresoares.eu@gmail.com>
2023-10-17 11:38:30 +0100

    Em tempo

--- tp2/Professor/segment1.m
+++ tp2/Professor/segment1.m
@@ -9,64 +9,75 @@ clear all
 close all
 
 %color for plots
-dark_green = 1/255 * [0,100,0];
-dark_blue = 1/255 * [3,37,126];
+dark_green = 1/255 * [0, 100, 0];
+dark_blue = 1/255 * [3, 37, 126];
 
-filename=input('Nome do Ficheiro .wav? ','s');
-filename1=[filename,'.wav'];
-[sinal,Fa]=audioread(filename1);
+filename = input('Nome do Ficheiro .wav? ', 's');
+filename1 = [filename, '.wav'];
+[sinal, Fa] = audioread(filename1);
 
 % A Frequencia de Amostragem deve ser 22050 ou 44100 Hz
-if Fa==44100
-    sinal=decimate(sinal,2);
+if Fa == 44100
+    sinal = decimate(sinal, 2);
 end
-sinal=detrend(sinal);
-espacamento=10;
-energia=fenerg2(sinal,espacamento); % Energia do sinal
-m=fmedia(energia,5,1);energia=fmedia(m,3,1); % Alisamento
-LSES=max(energia(1:100));
-der=fderivad(sinal);
-zero=fzeros2(der,50,espacamento); % Taxa de passagem por zero da derivada
-m=fmedia(zero,5,1);zero=fmedia(m,3,1);  % Alisamento
-LIZ=min(zero(3:70)); % Limite Inferior da Taxa de Passagem por Zero
+sinal = detrend(sinal);
+espacamento = 10;
 
-car=zeros(round(length(sinal)/(espacamento*10)),2);
-s=zeros(round(length(sinal)/(espacamento*10)),1);
-decisao=zeros(round(length(sinal)/(espacamento*10)),1); 
+% Calculate time in seconds
+t = (1:length(sinal)) / Fa;
 
-for i=0:10:length(zero)-10
-  z=0;e=0;
-  for j=1:10  
-    if zero(i+j)>LIZ, z=z+1; end
-    if energia(i+j)<LSES, e=e+1; end
-  end
-  car(i/10+1,1)=e;   % Energia
-  car(i/10+1,2)=z;   % Taxa de Passsagem por zero
+energia = fenerg2(sinal, espacamento); % Energia do sinal
+m = fmedia(energia, 5, 1);
+energia = fmedia(m, 3, 1); % Alisamento
+LSES = max(energia(1:100));
+
+der = fderivad(sinal);
+zero = fzeros2(der, 50, espacamento); % Taxa de passagem por zero da derivada
+m = fmedia(zero, 5, 1);
+zero = fmedia(m, 3, 1); % Alisamento
+LIZ = min(zero(3:70)); % Limite Inferior da Taxa de Passagem por Zero
+
+car = zeros(round(length(sinal) / (espacamento * 10)), 2);
+s = zeros(round(length(sinal) / (espacamento * 10)), 1);
+decisao = zeros(round(length(sinal) / (espacamento * 10)), 1);
+
+for i = 0:10:length(zero) - 10
+    z = 0;
+    e = 0;
+    for j = 1:10
+        if zero(i + j) > LIZ, z = z + 1; end
+        if energia(i + j) < LSES, e = e + 1; end
+    end
+    car(i / 10 + 1, 1) = e; % Energia
+    car(i / 10 + 1, 2) = z; % Taxa de Passsagem por zero
 end
-s=sum(car,2);
+s = sum(car, 2);
+
 % Matriz de decisão com 3 classes
-% Decisao= 0->nao definido; 1-> silencio; 3-> excitado por ruido (noise); 4-> vocalizado;
+% Decisao = 0 -> nao definido; 1 -> silencio; 3 -> excitado por ruido (noise); 4 -> vocalizado;
 
-for i=1:length(decisao) 
- if s(i)>=14, decisao(i)=1;
-   elseif s(i)<6, decisao(i)=4;
-     elseif (car(i,1)<3 && car(i,2)>5), decisao(i)=3;
- end
-end
-for i=3:length(decisao) 
-    if decisao(i-2)==decisao(i) 
-        decisao(i-1)=decisao(i); 
+for i = 1:length(decisao)
+    if s(i) >= 14, decisao(i) = 1;
+    elseif s(i) < 6, decisao(i) = 4;
+    elseif (car(i, 1) < 3 && car(i, 2) > 5), decisao(i) = 3;
     end
 end
-figure;plot((1:length(sinal)),sinal/max(sinal)*4,'y',100*(1:length(decisao)),decisao, '.');
+for i = 3:length(decisao)
+    if decisao(i - 2) == decisao(i)
+        decisao(i - 1) = decisao(i);
+    end
+end
+
+figure;
+plot(t, sinal / max(sinal) * 4, 'y', 100 * (1:length(decisao)) / Fa, decisao, '.'); xlim([0, 21]);
 title('Classificação: 0 - Indefinido, 1 - Silêncio, 3 - Ruido e 4 - Voz');
-binEdges = 0:0.5:5;
-% Crie alguns dados de exemplo
-x = 0.3:1:4.3;
-% Defina os rótulos personalizados para o eixo x
-novo_rotulo_x = {'Indefinido', 'Silêncio', '', 'Ruido', 'Voz'};
 
 
+
+figure; plot(t, sinal/max(sinal)*4, 'y', 100*(1:length(decisao)) / Fa, decisao, '.'); xlim([15, 16]);
+title('Classificação: 0 - Indefinido, 1 - Silêncio, 3 - Ruido e 4 - Voz');
+
+binEdges = 0:0.5:5; x = 0.3:1:4.3; novo_rotulo_x = {'Indefinido', 'Silêncio', '', 'Ruido', 'Voz'}; % Rotulos
 figure(7); histogram(decisao, binEdges, 'FaceColor', dark_blue); ylabel('Total de ocorrências'); xticks(x); xticklabels(novo_rotulo_x);
 title('Número de ocorrências de Indefinido, Silêncio, Ruido e Voz');
 
@@ -89,14 +100,13 @@ for i=3:length(decisao)
          decisao(i-1)=decisao(i); 
      end
 end
-figure;plot((1:length(sinal)),sinal/max(sinal)*4,'y',100*(1:length(decisao)),decisao,'.');
+figure;plot(t,sinal/max(sinal)*4,'y',100*(1:length(decisao)) / Fa,decisao,'.'); xlim([0, 21]);
 title('Classificação: 0-Indef. 1-Silê. 2-Ruído 2.5-Comum; 3-Mix 4-Voz');
 
-binEdges = 0:0.5:5;
-% Crie alguns dados de exemplo
-x = [0.3, 1.3, 2.3, 2.81, 3.3, 4.3]; %0.3:1:4.3;
-% Defina os rótulos personalizados para o eixo x
-novo_rotulo_x = {'Indefinido', 'Silêncio', 'Ruido', 'Normal', 'Mix', 'Voz'};
+figure; plot(t, sinal/max(sinal)*4, 'y', 100*(1:length(decisao)) / Fa, decisao, '.');xlim([15, 16]);
+title('Classificação: 0-Indef. 1-Silê. 2-Ruído 2.5-Comum; 3-Mix 4-Voz');
+
+binEdges = 0:0.5:5; x = [0.3, 1.3, 2.3, 2.81, 3.3, 4.3]; novo_rotulo_x = {'Indefinido', 'Silêncio', 'Ruido', 'Normal', 'Mix', 'Voz'};
 figure(8); histogram(decisao, binEdges, 'FaceColor', dark_blue); ylabel('Total de ocorrências'); xticks(x); xticklabels(novo_rotulo_x);
 title('Número de ocorrências de Indefinido, Silêncio, Ruido, Comum, Mix e Voz');
 
