b439636a4a5f5ce4ed0d805fd757ba237e283ba0
AndBarata <146003937+AndBarata@users.noreply.github.com>
2023-11-29 11:55:37 +0000

    update da tp2

--- projetoFinal/identificacaoNotas.py
+++ projetoFinal/identificacaoNotas.py
@@ -1,12 +1,10 @@
 import numpy as np
-import threading
-import pyaudio
+import sounddevice as sd
 from scipy.signal import find_peaks
 
-
 def calculateF0(audioData, fs):
-    f0_min = 20; #Hz
-    f0_max = 10000; #Hz
+    f0_min = 20  # Hz
+    f0_max = 10000  # Hz
 
     # Calculate the autocorrelation of the signal
     correlation = np.correlate(audioData, audioData, mode='full')
@@ -16,45 +14,33 @@ def calculateF0(audioData, fs):
     peaks, _ = find_peaks(correlation)
 
     # Find the index of the highest peak
-    highest_peak_index = np.argmax(peaks)
+    highest_peak_index = peaks[np.argmax(correlation[peaks])]
 
     # Calculate the fundamental frequency
     f0 = fs / (highest_peak_index + 1)
 
     return f0
 
+def get_audio_data(fs):
+    seconds = 1  # Duration of recording
 
+    audio_data = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
+    sd.wait()  # Wait for the recording to finish
 
-def get_audio_data():
-    # Set up PyAudio
-    p = pyaudio.PyAudio()
+    if audio_data.ndim > 1:
+        audio_data = np.mean(audio_data, axis=1)
 
-    # Open stream
-    stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
-
-    # Read from the stream
-    audio_data = np.fromstring(stream.read(1024), dtype=np.int16)
-
-    # Close the stream
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-    return audio_data
+    return np.array(audio_data)
 
 # Main function
 if __name__ == "__main__":
-    fs = 44100 #Hz
+    fs = 44100  # Hz
 
     while True:
         # Get audio data from microphone
-        audioData = get_audio_data()
+        audioData = get_audio_data(fs)
 
         # Calculate the fundamental frequency
         note = calculateF0(audioData, fs)
-        
+
         print(note)
-
-
-
-
