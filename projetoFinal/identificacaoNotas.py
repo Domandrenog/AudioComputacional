00ec97ee07401ede8d1897fe409e7f3b8a7d229e
AndBarata <146003937+AndBarata@users.noreply.github.com>
2023-11-29 16:50:11 +0000

    Projeto deteta notas em tempo real com threads

--- projetoFinal/identificacaoNotas.py
+++ projetoFinal/identificacaoNotas.py
@@ -1,6 +1,12 @@
 import numpy as np
 import sounddevice as sd
 from scipy.signal import find_peaks
+import threading
+import queue
+import time  
+
+audioData = []
+f0_global = 0
 
 def calculateF0(audioData, fs):
     f0_min = 20  # Hz
@@ -19,28 +25,37 @@ def calculateF0(audioData, fs):
     # Calculate the fundamental frequency
     f0 = fs / (highest_peak_index + 1)
 
-    return f0
+    print(f0)
 
-def get_audio_data(fs):
-    seconds = 1  # Duration of recording
 
-    audio_data = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
-    sd.wait()  # Wait for the recording to finish
+def get_audio_data(fs, audio_queue):
+    while True:
+        seconds = 1  # Duration of recording
+        audio_data = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
+        sd.wait()  # Wait for the recording to finish
 
-    if audio_data.ndim > 1:
-        audio_data = np.mean(audio_data, axis=1)
+        if audio_data.ndim > 1:
+            audio_data = np.mean(audio_data, axis=1)
 
-    return np.array(audio_data)
+        audio_queue.put(np.array(audio_data))
+
+def process_audio_data(fs, audio_queue):
+    while True:
+        audio_data = audio_queue.get()
+        note = calculateF0(audio_data, fs)
+        print(note)
 
 # Main function
 if __name__ == "__main__":
     fs = 44100  # Hz
+    audio_queue = queue.Queue()
 
-    while True:
-        # Get audio data from microphone
-        audioData = get_audio_data(fs)
 
-        # Calculate the fundamental frequency
-        note = calculateF0(audioData, fs)
+    audio_thread = threading.Thread(target=get_audio_data, args=(fs, audio_queue))
+    processing_thread = threading.Thread(target=process_audio_data, args=(fs, audio_queue))
 
-        print(note)
+    audio_thread.start()
+    processing_thread.start()
+
+    audio_thread.join()
+    processing_thread.join()
